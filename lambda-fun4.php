082a70d47b918f4edab6056e7873d68307b46428
oguzyildiz1 <yildizoguz1@gmail.com>
2025-01-13 11:50:36 +0300

    updated

---
+++ lambda-fun4.php
@@ -0,0 +1,78 @@
+<?php
+$authors = [
+    [
+        'name' => 'Stephen King',
+        'book' => 'It',
+        'year' => 2001,
+        'store' => 'http://www.amazon.com'
+    ],
+    [
+        'name' => 'Jrr Tolkien',
+        'book' => 'Lotr',
+        'year' => 1990,
+        'store' => 'http://www.amazon.com'
+
+    ],
+    [
+        'name' => 'R. A. Salvatore',
+        'book' => 'Drizzt Do\'urden',
+        'year' => 2010,
+        'store' => 'http://www.alibaba.com'
+
+    ],
+    [
+        'name' => 'Stephen King',
+        'book' => 'Kara Kule',
+        'year' => 2010,
+        'store' => 'http://www.alibaba.com'
+    ]
+];
+
+// $items is the array
+$filter = function ($items, $fn) {
+    $filteredItems = [];
+    // search for amazon
+    foreach ($items as $item) {
+        // check seller, if amazon, add that to new arr
+        if ($fn($item)) {
+            $filteredItems[] = $item;
+        }
+    }
+
+    return $filteredItems;
+    // returns an array
+};
+
+$filteredAuthors = $filter($authors, function ($author) {
+    return $author["year"] < 2010;
+});
+
+
+// <?php foreach ($filteredAuthors) :
+//    <li>a</li>
+// endforeach;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assoc Arr</title>
+</head>
+
+<body>
+    <h1>Popular Books</h1>
+    <h3>Where to buy?</h3>
+    <ul>
+        <?php foreach ($filteredAuthors as $author) : ?>
+            <li><?= "{$author['name']} - {$author['book']} - {$author['year']}"; ?></li>
+        <?php endforeach; ?>
+    </ul>
+
+
+</body>
+
+</html>
---
+++ practices/lambda1.php
@@ -0,0 +1,25 @@
+<?php
+$cars = [
+    [
+        'name' => 'Ferrari',
+        'year' => 2001,
+        'price' => 500000
+    ],
+    [
+        'name' => 'Ferrari',
+        'year' => 2010,
+        'price' => 1500000
+
+    ],
+    [
+        'name' => 'Porsche',
+        'year' => 2010,
+        'price' => 900000
+
+    ],
+    [
+        'name' => 'Toyota',
+        'year' => 2015,
+        'price' => 250000
+    ]
+];
